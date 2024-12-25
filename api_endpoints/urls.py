e12a2a6cc3b0f010b33d36540275139083872658
Ozichi Orie <73189626+Ozichis@users.noreply.github.com>
2024-12-25 12:27:19 +0100

    Update urls.py

--- api_endpoints/urls.py
+++ api_endpoints/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('sign-up/', views.RegisterView.as_view(), name='sign_up_api'),
     path('verify/', views.VerifyEmailView.as_view(), name='verify_api'),
     path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
 
     path('upload_file/', views.FileUploadView.as_view(), name='api_upload_file'),
     path('download_file/', views.DownloadFile, name="download_f_api"), #fix
